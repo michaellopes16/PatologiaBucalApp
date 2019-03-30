cbcf4a70e606772b318a92a1c3b23952ea713b65
michaellopes16 <42485020+michaellopes16@users.noreply.github.com>
2019-03-30 02:13:52 -0300

    Add do gerenciador de lesões

--- public/public/js/index.js
+++ public/public/js/index.js
@@ -80,17 +80,88 @@ $("body").on('click', '#divLesoes button', function(){
     var id = $(this).attr('id');
     callAnothePage("casosClinicos.html", id);
 });  
-/*
-$("#bt_buscar_lesao").click(function(){
-    var nome = document.getElementById('idBucar').value;
-   alert(nome);
-    var desc = "descricao teste";
-    var cli = "Caso teste";
-    var img = "img teste";
-    salvarLesao(nome, desc, cli, img,"img2", "img3",  "radio", "histo", "trata", "diag" );
 
+if(document.getElementById('upImagemClinica'))
+{
+
+    var upImagemClinica = document.getElementById('upImagemClinica');
+    var upImagemHisto   = document.getElementById('upImagemHisto');
+    var upImagemRadio   = document.getElementById('upImagemRadio');
+    var uploader1   = document.getElementById('uploader1');
+    var uploader2   = document.getElementById('uploader2');
+    var uploader3   = document.getElementById('uploader3');
+
+    upImagemClinica.addEventListener('change', function(e){
+      //Get File
+      var file = e.target.files[0]; 
+      console.log('File available at', saveImage(file, uploader1, "URL_img1"));
+    });
+    upImagemHisto.addEventListener('change', function(e){
+      //Get File
+      var file = e.target.files[0]; 
+      console.log('File available at', saveImage(file, uploader2, "URL_img2"));
+    });
+    upImagemRadio.addEventListener('change', function(e){
+      //Get File
+      var file = e.target.files[0]; 
+      console.log('File available at', saveImage(file, uploader3, "URL_img3"));
+    });
+}
+
+function saveImage(file, uploader1, storageImg)
+{
+ // var urlRetorno ='';
+  //Create a storage reference
+  var storagaRef = firebase.storage().ref('imagens_clinicas/'+file.name);
+  
+  //upload file
+  var task = storagaRef.put(file);
+  
+  //upload progress bar
+  task.on('state_changed',
+
+    function progress(snapshot){
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) *100;
+      uploader1.value = percentage;
+    },
+    function error (err){
+
+    },
+
+    function complete(){
+      // Upload completed successfully, now we can get the download URL
+          task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            //console.log('File available at', downloadURL);
+            window.localStorage.setItem(storageImg, downloadURL);
+          }
+        );
+    }, 
+  );
+
+
+   
+}
+
+
+$("#bt_salvar_lesao").click(function(){
+
+   var nome = document.getElementById('idNome').value;
+   var descricao = document.getElementById('idDescricao').value;
+   var clinico = document.getElementById('idClinico').value;
+   var histologico = document.getElementById('idHistologico').value;
+   var diagnostico = document.getElementById('idDiagnostico').value;
+   var radiologico = document.getElementById('idRadiologico').value;
+   var tratamento = document.getElementById('idTratamento').value;
+   img = window.localStorage.getItem("URL_img1");
+   img2 = window.localStorage.getItem("URL_img2");
+   img3 = window.localStorage.getItem("URL_img3");
+    
+
+   salvou = salvarLesao(nome, descricao, clinico, img,img2, img3,  radiologico, histologico, tratamento, diagnostico);
+   callAnothePage("lesoes.html")
+   alert("Lesão salva com sucesso!");
 });
-*/
+
 function salvarLesao(nome, desc, cli, img,img2,img3, radio, histo, trata, diag ) {
   // A post entry.
     // Get a key for a new Post.
